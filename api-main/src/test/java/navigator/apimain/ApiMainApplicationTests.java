e38300a36f8baa752b3418b3a01c7ed536552ba0
HwanJung <jh013188@gmail.com>
2026-03-05 15:57:13 +0900

    Add initial spring boot project

---
+++ api-main/src/test/java/navigator/apimain/ApiMainApplicationTests.java
@@ -0,0 +1,13 @@
+package navigator.apimain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
