e90d647e1b90e3b1aeb2cbf875990e78a281702e
TaegyuKim-byte <yjh5331ang@gmail.com>
2026-03-09 16:32:52 +0900

    chore: set up project base package to com.project.knowledge

--- api-main/src/main/java/com/project/knowledge/ApiMainApplication.java
+++ api-main/src/main/java/com/project/knowledge/ApiMainApplication.java
@@ -1,4 +1,4 @@
-package navigator.apimain;
+package com.project.knowledge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
