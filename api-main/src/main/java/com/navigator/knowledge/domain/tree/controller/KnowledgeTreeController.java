e8a9f8759a864db7629c33292eec48bb57941e36
HwanJung <jh013188@gmail.com>
2026-04-08 02:36:50 +0900

    refactor: rename a knowledge tree endpoint

--- api-main/src/main/java/com/navigator/knowledge/domain/tree/controller/KnowledgeTreeController.java
+++ api-main/src/main/java/com/navigator/knowledge/domain/tree/controller/KnowledgeTreeController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/tree")
+@RequestMapping("/api/tree")
 @RequiredArgsConstructor
 public class KnowledgeTreeController {
 
