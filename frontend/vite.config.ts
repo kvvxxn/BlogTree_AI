26d46fe01f273acfb1b68b5cee6afc8fa46922a1
kvvxxn <kwondh3236@naver.com>
2026-04-25 12:22:39 +0900

    Change config and path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": new URL("./src", import.meta.url).pathname,
+      // 윈도우에서 에러가 나는 new URL 대신, 안전한 path.resolve를 사용합니다.
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
